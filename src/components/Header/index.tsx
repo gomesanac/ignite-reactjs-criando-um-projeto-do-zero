5cf6c307c100e1506a1c7c211984030ddf4fabdd
Ana Carolina Gomes <anagomes@positivosolucoes.com.br>
2022-03-23 17:49:23 -0300

    feat: implementa header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={commonStyles.container}>
+      <div className={`${commonStyles.content} ${styles.header}`}>
+        <Link href="/">
+          <img src="/images/logo.svg" alt="logo" />
+        </Link>
+      </div>
+    </header>
+  );
 }
